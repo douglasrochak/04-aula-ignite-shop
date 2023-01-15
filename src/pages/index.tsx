f67dbd964c2d1b39268367eb058324adaabf9eb0
Rochak <douglas.srochak@gmail.com>
2023-01-15 20:39:50 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$green500",
+  padding: "4px 8px",
+  borderRadius: 4,
+  border: 0,
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
+export default function Home() {
+  return (
+    <Button>
+      <span>Teste </span>Enviar
+    </Button>
+  );
+}
---
+++ src/pages/success.tsx
@@ -0,0 +1,3 @@
+export default function Success() {
+  return <h1>Pagina de sucesso</h1>;
+}
